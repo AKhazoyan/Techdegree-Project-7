a66d4348090e8b7fe25ef3e3d5e1573b86e1dea0
AKhazoyan <52087197+AKhazoyan@users.noreply.github.com>
2020-10-22 21:51:43 -0500

    Expanded autocomplete

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ closeButton.addEventListener('click', function() {
 })
 
 
+//Top Search
+let topSearchField = document.querySelector('#searchField');
+let appUsers = ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'];
+
+document.querySelector("#searchButton").addEventListener('click', function() {
+	if (topSearchField.value === "") {	
+		alert('Please type in a name to search.');
+	} else if (appUsers.indexOf(topSearchField.value) >= 0) {
+		alert('User: ' + topSearchField.value + ' exists!');
+	} else {
+		alert('User: ' + topSearchField.value + ' doesn\'t exist.');
+	};
+	topSearchField.value = "";
+});
+
+
 
 //Traffic Chart Line Graph Widget//
 
@@ -289,63 +305,81 @@ let mobileChart = new Chart(mobileCanvas, {
 
 // Messaging Section //
 
+	document.querySelector("#searchField").addEventListener("click", function() {
+		document.querySelector("#searchFieldSuggestions").style.display = "block";
+	});
+
 	document.querySelector("#userField").addEventListener("click", function() {
-		document.querySelector("#usersNames").style.display = "block";
+		document.querySelector("#userFieldSuggestions").style.display = "block";
 	});
 
 	function hideAutocompleteResults() {
-		document.querySelector("#usersNames").style.display = "none";
+		document.querySelector("#searchFieldSuggestions").style.display = "none";
+		document.querySelector("#userFieldSuggestions").style.display = "none";
 	}
 
-	document.querySelector("#userField").addEventListener("blur", function() {
+	document.querySelector("#searchField").addEventListener("blur", function() {
 			hideAutocompleteResults();
 	});
 
+	document.querySelector("#userField").addEventListener("blur", function() {
+		hideAutocompleteResults();
+	});
+
 	//Autocomplete Member Search//
+
+	document.querySelector("#searchField").addEventListener("autoComplete", event => {
+		console.log(event);
+	});
 
 	document.querySelector("#userField").addEventListener("autoComplete", event => {
 		console.log(event);
 	});
 
-	new autoComplete({
-		data: {                              // Data src [Array, Function, Async] | (REQUIRED)
-		  src: ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'],
-		//   key: ["title"],
-		  cache: true
-		},
-		sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
-			if (a.match < b.match) return -1;
-			if (a.match > b.match) return 1;
-			return 0;
-		},
-		placeHolder: "Search for User",      // Place Holder text                 | (Optional)
-		selector: "#userField",              // Input field selector              | (Optional)
-		threshold: 1,                        // Min. Chars length to start Engine | (Optional)
-		debounce: 300,                       // Post duration for engine to start | (Optional)
-		searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-		resultsList: {                       // Rendered results list object      | (Optional)
-			render: true,
-			container: source => {
-				source.setAttribute("id", "usersNames");
+	function createAutoComplete(searchBox, idName) {
+		new autoComplete({
+			data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+			src: ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'],
+			//   key: ["title"],
+			cache: true
 			},
-			destination: document.querySelector("#userField"),
-			position: "afterend",
-			element: "ul"
-		},
-		maxResults: 5,                         // Max. number of rendered results | (Optional)
-		highlight: true,                       // Highlight matching results      | (Optional)
-		resultItem: {                          // Rendered result item            | (Optional)
-			content: (data, source) => {
-				source.innerHTML = data.match;
+			sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+				if (a.match < b.match) return -1;
+				if (a.match > b.match) return 1;
+				return 0;
 			},
-			element: "li"
-		},
-		onSelection: feedback => {             // Action script onSelection event | (Optional)
-			let selection = feedback.selection.value;
-			document.querySelector("#userField").value = selection;
-		},
-		
-	});
+			placeHolder: "Search for User",      // Place Holder text                 | (Optional)
+			selector: searchBox,                 // Input field selector              | (Optional)
+			threshold: 1,                        // Min. Chars length to start Engine | (Optional)
+			debounce: 300,                       // Post duration for engine to start | (Optional)
+			searchEngine: "strict",              // Search Engine type/mode           | (Optional)
+			resultsList: {                       // Rendered results list object      | (Optional)
+				render: true,
+				container: source => {
+					source.setAttribute("id", idName);
+				},
+				destination: document.querySelector(searchBox),
+				position: "afterend",
+				element: "ul"
+			},
+			maxResults: 5,                         // Max. number of rendered results | (Optional)
+			highlight: true,                       // Highlight matching results      | (Optional)
+			resultItem: {                          // Rendered result item            | (Optional)
+				content: (data, source) => {
+					source.innerHTML = data.match;
+				},
+				element: "li"
+			},
+			onSelection: feedback => {             // Action script onSelection event | (Optional)
+				let selection = feedback.selection.value;
+				document.querySelector(searchBox).value = selection;
+			},
+		});
+	};
+
+	createAutoComplete("#searchField", "searchFieldSuggestions");
+	createAutoComplete("#userField", "userFieldSuggestions");
+
 
 
 
